1f55d54aaa783d7e2fb7daa3b5d4b08560a60281
rexyuliguo <993493097@qq.com>
2017-02-17 16:13:50 +0800

    modified

--- src/com/rex/snake/MoveTimer.java
+++ src/com/rex/snake/MoveTimer.java
@@ -3,27 +3,27 @@ package com.rex.snake;
 import java.util.TimerTask;
 
 public class MoveTimer extends TimerTask {
-    @Override
-    public void run() {
-        if (Snake.right == 2) {
-            Snake.right = 0;
-            System.out.println("Right set to 0.");
-            Keys.rightT.cancel();
-        }
-        if (Snake.left == 2) {
-            Snake.left = 0;
-            System.out.println("Left set to 0.");
-            Keys.leftT.cancel();
-        }
-        if (Snake.down == 2) {
-            Snake.down = 0;
-            System.out.println("Down set to 0.");
-            Keys.downT.cancel();
-        }
-        if (Snake.up == 2) {
-            Snake.up = 0;
-            System.out.println("Up set to 0.");
-            Keys.upT.cancel();
-        }
-    }
+	@Override
+	public void run() {
+		if (Snake.right == 2) {
+			Snake.right = 0;
+			System.out.println("Right set to 0.");
+			Keys.rightT.cancel();
+		}
+		if (Snake.left == 2) {
+			Snake.left = 0;
+			System.out.println("Left set to 0.");
+			Keys.leftT.cancel();
+		}
+		if (Snake.down == 2) {
+			Snake.down = 0;
+			System.out.println("Down set to 0.");
+			Keys.downT.cancel();
+		}
+		if (Snake.up == 2) {
+			Snake.up = 0;
+			System.out.println("Up set to 0.");
+			Keys.upT.cancel();
+		}
+	}
 }
